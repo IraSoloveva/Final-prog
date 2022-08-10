94c4cd482fa90605fc3530b439072e8617be7885
soloveva <solovevaia@susu.ru>
2022-08-10 12:25:48 +0500

    task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введеите трехзначное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number1 / 100;
+int result = (number1 - number2 * 100) / 10;
+
+
+Console.Write("Вторая цифра данного трехзначного числа: ");
+Console.WriteLine(result);
+
+
+

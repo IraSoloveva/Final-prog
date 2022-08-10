9b89144277f4b982e7417d1ae7b6a5f33113f0ab
soloveva <solovevaia@susu.ru>
2022-08-10 17:43:48 +0500

    task 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введеите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int d =100;
+int c = number1 / d;
+int pr = number1;
+
+if (c == 0)
+{
+    Console.Write("Такого числа нет");
+}
+else
+{
+    while (pr > 1000)
+    {
+     pr = pr /10;   
+    }
+    int t = pr/10;
+    int h = pr / 100;
+    Console.Write("Третья цифра данного числа: ");
+    Console.WriteLine(pr - t*10 - h/100);
+}

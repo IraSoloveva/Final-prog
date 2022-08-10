af48220174d1b4e6a6d0ba9498301aafc45a7d82
soloveva <solovevaia@susu.ru>
2022-08-10 17:53:26 +0500

    task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+Console.WriteLine("Введеите порядковый номер дня недели: 1 - понедельник, 2 - вторник, 3 - среда, 4 - четверг, 5 - пятница, 6 - суббота, 7 - воскресенье");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Этот день выходной? ");
+
+if (numberDay < 6)
+{
+    Console.Write("Нет");
+}
+else
+{
+    Console.Write("Да");
+}

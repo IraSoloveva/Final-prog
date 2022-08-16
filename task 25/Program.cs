47b517aff30d01a1597c0997dcbe08972dcce4f3
soloveva <solovevaia@susu.ru>
2022-08-17 00:56:17 +0500

    task 25

---
+++ task 25/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int[] numbers = new int[2];
+
+Console.WriteLine("Введите 2 числа");
+Console.WriteLine("Число А:");
+numbers[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число B: ");
+numbers[1] = Convert.ToInt32(Console.ReadLine());
+
+    int p = 1;
+    for (int i = 1; i <= numbers[1]; i++)
+    {
+        p = p * numbers[0];
+    }
+  Console.WriteLine(p);  
+
+
+

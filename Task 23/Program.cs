efcdb75ce2f5aceaf02f1532a3948ebb98bb9eb4
soloveva <solovevaia@susu.ru>
2022-08-16 22:22:14 +0500

    task 23

---
+++ Task 23/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введеите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int i = 1;
+        while (i <= number)
+        {
+            Console.Write(Math.Pow(i, 3));
+            Console.Write(",");
+            i = i+1;
+        }
+    }
+}

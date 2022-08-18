04adec87dbaa8805c3ee42923713298020c99d78
soloveva <solovevaia@susu.ru>
2022-08-18 23:05:39 +0500

    task 29

---
+++ task 29/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+System.Console.WriteLine("Введите несколько чисел, исползуя для их разделения пробел");
+
+string[] nums_strings = Console.ReadLine().Split();
+
+int[] nums = new int[nums_strings.Length];
+
+for (int i = 0; i < nums_strings.Length; i++)
+{
+    nums[i] = Convert.ToInt32(nums_strings[i]);
+}
+for (int i = 0; i < nums_strings.Length; i++)
+{
+    Console.Write(nums[i] + " ");
+}

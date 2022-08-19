e2526f6923aa9980cb4a057d75970606fb09055f
soloveva <solovevaia@susu.ru>
2022-08-19 11:14:54 +0500

    task 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введеите  число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int d = number;
+
+while (d > 10)
+{
+    int i2 = d % 10;
+    sum = sum + i2;
+    d = d / 10;
+}
+Console.WriteLine(d);
+sum = sum + d;
+Console.WriteLine("Сумма цифр введенного числа равна:");
+Console.WriteLine(sum);
+
+
+

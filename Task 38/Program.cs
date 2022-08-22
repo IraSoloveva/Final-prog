5da6225bfede0a93e9d92e1b3ead891d8ddcfe49
soloveva <solovevaia@susu.ru>
2022-08-22 10:36:01 +0500

    task 38

---
+++ Task 38/Program.cs
@@ -0,0 +1,32 @@
+﻿
+System.Console.WriteLine("Введите желаемый размер массива");
+
+int length = Convert.ToInt32(Console.ReadLine());
+
+double[] nums = new double[length];
+int i = 0;
+
+while (i < length)
+{
+    nums[i] = new Random().Next(0, 100);
+    i++;
+}
+
+for (i = 0; i < length; i++)
+{
+    Console.Write(nums[i] + " ");
+}
+
+double min = nums[0];
+double max = nums[0];
+for (i = 0; i < length; i++)
+{
+    if (nums[i] > max)
+        max = nums[i];
+        
+        if (nums[i] < min)
+        min = nums[i];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементами массива: ");
+Console.Write(max-min);

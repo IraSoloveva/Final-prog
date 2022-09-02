468c39c6d3fdfd3c0d57fedc9e883edb6d0d3f6a
soloveva <solovevaia@susu.ru>
2022-09-02 10:39:25 +0500

    task 47

---
+++ Task 47/Program.cs
@@ -0,0 +1,31 @@
+﻿
+System.Console.WriteLine("Введите желаемый размер массива");
+System.Console.WriteLine("Количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] nums = new double[m, n];
+
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        nums[i, j] = new Random().Next(0, 100);
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{nums[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+

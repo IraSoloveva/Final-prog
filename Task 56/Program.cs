62bb8ada0f5f5a0134b2f2603588eef9468ee0f4
soloveva <solovevaia@susu.ru>
2022-09-03 15:20:35 +0500

    task 56

---
+++ Task 56/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+System.Console.WriteLine("Введите желаемый размер прямоугольного массива");
+System.Console.WriteLine("Количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] nums = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        nums[i, j] = new Random().Next(0, 100);
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{nums[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int Sum (int[] matr)
+{
+     int sum = 0;
+     for (int i = 0; i < n; i++)
+    {
+       sum = sum + matr[i];
+    }
+    return sum;
+}
+
+Console.WriteLine();
+System.Console.WriteLine("Номер строки с наименьшей суммой значений элементов: ");
+
+int[] result = new int[m];
+
+for (int i = 0; i < m; i++)
+{
+    int[] nums2 = new int[n];
+    for (int j = 0; j < n; j++)
+    {
+        nums2[j] = nums[i, j];
+    } 
+    result[i]=Sum(nums2);
+}
+    int min=0;
+for (int i=0; i<m; i++)
+    if (result[i]<result[min])
+    {
+        min=i;
+     }   
+        
+        Console.Write(min+1);
+    Console.WriteLine();
+
+
+
+
+

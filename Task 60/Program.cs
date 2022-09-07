cbbc51c0f237b557d792d3767c1fa5577184078d
soloveva <solovevaia@susu.ru>
2022-09-07 10:24:18 +0500

    task 60

---
+++ Task 60/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+int[,,] nums = new int[2, 2, 2];
+
+
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k=0; k<2; k++)
+            {
+                nums[i, j, k]= new Random().Next(10, 99);
+                
+            }
+        }
+    }
+
+
+    Console.WriteLine("ТТрехмерный массив:  ");
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int l = 0; l < 2; l++)
+            {
+                Console.Write($"{nums[i, j, l]} ({i}, {l}, {j}) ");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+    

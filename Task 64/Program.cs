0a169054e1ba541a6bcccb2e8fc6c4c135064c29
soloveva <solovevaia@susu.ru>
2022-09-08 21:42:43 +0500

    task 64

---
+++ Task 64/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Задайте первое число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Задайте второе число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ряд натуральных чисел, расположенных между числами M и N: ");
+
+if (m > n)
+{
+    for (int i = n; i <= m; i++)
+        System.Console.WriteLine(i);
+}
+else
+{
+    for (int i = m; i <= n; i++)
+        System.Console.WriteLine(i);
+}
+
+
+

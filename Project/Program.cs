4bf19b89b7ddac4c78ccbe98e860c4779d8a8ba7
soloveva <solovevaia@susu.ru>
2022-09-21 00:23:56 +0500

    Задача итогового задания

---
+++ Project/Program.cs
@@ -0,0 +1,18 @@
+﻿
+System.Console.WriteLine("Введите несколько чисел, исползуя для их разделения пробел");
+
+string[] nums_strings = Console.ReadLine().Split();
+
+string[] nums = new string[nums_strings.Length];
+
+for (int i = 0; i < nums_strings.Length; i++)
+{
+    nums[i] = Convert.ToString(nums_strings[i]);
+}
+
+for (int i = 0; i < nums_strings.Length; i++)
+{
+   if (nums[i].Length <=3)
+    Console.Write(nums[i] + " ");
+}
+
